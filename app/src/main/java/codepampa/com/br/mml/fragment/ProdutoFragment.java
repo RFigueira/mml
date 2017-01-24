051505bcfc804eac3d840b3c67355c45b8def6a7
Rodrigo <rodrigo.freitas@tecsinapse.com.br>
2017-01-23 22:26:54 -0200

    atualização - 2

--- app/src/main/java/codepampa/com/br/mml/fragment/ProdutoFragment.java
+++ app/src/main/java/codepampa/com/br/mml/fragment/ProdutoFragment.java
@@ -2,14 +2,18 @@ package codepampa.com.br.mml.fragment;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.math.BigDecimal;
 
 import codepampa.com.br.mml.R;
 import codepampa.com.br.mml.activity.ProdutoActivity;
@@ -43,6 +47,35 @@ public class ProdutoFragment extends BaseFragment {
         return  view;
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuitem_salvar:{
+
+                popularProduto();
+//                new CarrosTask().execute(SAVE); //executa a operação CREATE em uma thread AsyncTask
+//                break;
+
+            }
+            case R.id.menuitem_excluir:{
+                /*new CarrosTask().execute(DELETE); //executa a operação DELETE em uma thread AsyncTask
+                break;*/
+            }
+            case android.R.id.home:
+                getActivity().finish();
+                break;
+        }
+        return false;
+    }
+
+    private void popularProduto() {
+        produto.nome = editTextNomeProduto.getText().toString();
+        produto.marca = editTextMarca.getText().toString();
+        produto.localCompra = editTextLocalCompra.getText().toString();
+        produto.preco = new BigDecimal(editTextValorProduto.getText().toString());
+    }
+
     private void editarProduto(View view) {
     }
 
@@ -50,13 +83,29 @@ public class ProdutoFragment extends BaseFragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-       // inflater.inflate(R.menu.activity_main_drawer, menu);
+        inflater.inflate(R.menu.menu_fragment_acoes, menu);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == getActivity().RESULT_OK){
+            Uri arquivoUri = data.getData();
+
+            if(arquivoUri.toString().contains("images")) {
+                imageViewFoto.setImageURI(arquivoUri);
+                produto.urlImagem = arquivoUri.toString();
+            }
+
+        }
+
     }
 
     private void acoes(View view) {
 
         if(Util.isObjectsNull(produto)) {
-
             produto = new Produto();
             novoProduto(view);
         } else {
